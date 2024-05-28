12159ff1ec31856342773b64203655fc44321111
Impietuoso <palidoman@gmail.com>
2024-05-28 01:53:22 -0300

    Pequenos AJustes de Canoa

--- Dragon of the Sea/Assets/Scripts/Progression/Jangada.cs
+++ Dragon of the Sea/Assets/Scripts/Progression/Jangada.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Jangada : MonoBehaviour
 {
+    public Menu menu;
     public void Partir() {
         if (Player.instance.save.rescueComplete) {
             Invoke("StopPlayer", 0.1f);
         }
+    }
+
+    public void Island(int scene) {
+        menu.LoadScene(scene);
     }
 
     void StopPlayer() {
